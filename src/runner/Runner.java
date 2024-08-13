296507bfdcb59088416fdfd5a2b98186fab4334b
jQrgitQ <158132525+jQrgitQ@users.noreply.github.com>
2024-08-13 02:26:36 +0200

    Arreglando Runner planificacion docente

--- src/runner/Runner.java
+++ src/runner/Runner.java
@@ -32,8 +32,6 @@ public class Runner {
 	public static DefaultTableModel modeloGrupoReporte;
 	public static DefaultTableModel modeloEstudianteReporte;
 	public static DefaultTableModel modeloPlanDocente;
-	public static ArrayList<Estudiante> estudiantes;
-	public static ArrayList<PersonalApoyo> personalAux;
 	public static ArrayList<Asignatura> asignaturas;
 	public static ArrayList<Estudiante> estudiantesReporte;
 	public static ArrayList<Grupo> gruposReportes;
@@ -57,61 +55,27 @@ public class Runner {
 		fct.crearPersona("05022358174", "Jorge Castro Pérez","Máster","Asistente","Cujae","MINES", "Calle 25 entre 21 y Ave. 26");
 		fct.crearPersona("05062347564", "Manuel Castro Reyes","Máster","Titular","Cujae","MINES", "Calle 30 entre 34 y Ave. 56");
 		
-	
 		DatosAuto.definirTablaProfes(fct.buscarProfesores());
 		
-		//ESTUDIANTES
-		Estudiante estu1 = new Estudiante("05032379581", "Rafael Menéndez Rodriguez",1,"Sucursal Comercial #5","Etecsa", "Calle 30 entre 34 y Ave. 56");
-		Estudiante estu2 = new Estudiante("08868513264", "Alejandro González Fernández",1,"La Mariposa","TRD","Ave. 26 entre calles A y B");
-		Estudiante estu3 = new Estudiante("04021324587", "Jorgito", 2, "Las Palamas", "CTC", "Tulipan y Boyeros");
-			
-		
+
 		// JORGITOOOOOOO ---->>>>    la creacion de los estudiantes por defecto
 		fct.crearPersona("05032379581", "Rafael11111 Menéndez Rodriguez", 1 ,"Sucursal Comercial #5","Etecsa", "Calle 30 entre 34 y Ave. 56");
 		fct.crearPersona("08868513264", "Alejandro González Fernández",1,"La Mariposa","TRD","Ave. 26 entre calles A y B");
 		fct.crearPersona("04021324587", "Jorgito", 2, "Las Palamas", "CTC", "Tulipan y Boyeros");
 		
 		
-		
-		
-		estudiantes = new ArrayList<Estudiante>();
-		estudiantes.add(estu1);
-		estudiantes.add(estu2);
-		estudiantes.add(estu3);
-		
 		DatosAuto.definirTablaEstudiantes(fct.buscarEstudiantes());
-		
-		
-		//Personal Auxiliar
-		PersonalApoyo persoAux1 = new PersonalApoyo("09062235147", "Federico Criado Domínguez","Laboratorio", "Calle 30 entre 34 y Ave. 56");
-		PersonalApoyo persoAux2 = new PersonalApoyo("59868285496", "Maria Elena Gómez Pérez","Biblioteca", "Ave. 26 entre calles A y B");
 					
 		
 		// JORGITOOOOOOO ---->>>>    la creacion del personal de apoyo por defecto
 		fct.crearPersona("09062235147", "Federico Criado Domínguez","Laboratorio", "Calle 30 entre 34 y Ave. 56");
 		fct.crearPersona("59868285496", "Maria Elena Gómez Pérez","Biblioteca", "Ave. 26 entre calles A y B");
 		
-		
-		
-		
-		
-		personalAux = new ArrayList<PersonalApoyo>();
-		personalAux.add(persoAux1);
-		personalAux.add(persoAux2);
 							
-		DatosAuto.definirTablaPersonalAux(personalAux);
-					
-		//Asignaturas
-		Asignatura asignatura1 = new Asignatura("Matemática I", 1, 1, 50);
-		Asignatura asignatura2 = new Asignatura("Matemática II", 1, 2, 50);
-		Asignatura asignatura3 = new Asignatura("Introducción a la Programación", 1, 1, 60);
-		Asignatura asignatura4 = new Asignatura("Diseño y POO", 1, 2, 90);
-		Asignatura asignatura5 = new Asignatura("Estructuras de Datos", 2, 1, 40);
-		Asignatura asignatura6 = new Asignatura("Seguridad Nacional", 2, 1, 90);
+		DatosAuto.definirTablaPersonalAux(fct.buscarPersonalApoyo());
 		
 		
-		
-		// JORGITOOOOOOO ---->>>>    la creacion del personal de apoyo por defecto
+		// JORGITOOOOOOO ---->>>>    la creacion de las asignaturas por defecto
 		fct.getPlanEstudio().crearAsignatura("Matemática I", 1, 1, 50);
 		fct.getPlanEstudio().crearAsignatura("Matemática II", 1, 2, 50);
 		fct.getPlanEstudio().crearAsignatura("Introducción a la Programación", 1, 1, 60);
@@ -119,24 +83,10 @@ public class Runner {
 		fct.getPlanEstudio().crearAsignatura("Estructuras de Datos", 2, 1, 40);
 		fct.getPlanEstudio().crearAsignatura("Seguridad Nacional", 2, 1, 90);
 		
-		
-					
-		asignaturas = new ArrayList<Asignatura>();
-		asignaturas.add(asignatura1);
-		asignaturas.add(asignatura2);
-		asignaturas.add(asignatura3);
-		asignaturas.add(asignatura4);
-		asignaturas.add(asignatura5);
-		asignaturas.add(asignatura6);
-							
-		DatosAuto.definirTablaPlanDeEstudio(asignaturas);
+		DatosAuto.definirTablaPlanDeEstudio(fct.getPlanEstudio().getAsignaturas());
 		
 		//ESTUDIANTE PARA REPORTES
-		estudiantesReporte = new ArrayList<Estudiante>();
-		estudiantesReporte.add(estu3);
-		estudiantesReporte.add(estu1);
-		
-		DatosAuto.definirTablaReportesEstu(estudiantesReporte);
+		DatosAuto.definirTablaReportesEstu(fct.buscarEstudiantesOros());
 
 		//GRUPOS PARA REPORTES
 		gruposReportes = new ArrayList<Grupo>();
