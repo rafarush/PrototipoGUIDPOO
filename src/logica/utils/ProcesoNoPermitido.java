cb5b634e4014b3a8a19563f836cc42d328e3bc60
jQrgitQ <158132525+jQrgitQ@users.noreply.github.com>
2024-09-08 17:22:46 +0200

    agregue las excepciones del empezar periodo MIS EXCEpciones

---
+++ src/logica/utils/ProcesoNoPermitido.java
@@ -0,0 +1,11 @@
+package logica.utils;
+
+public class ProcesoNoPermitido extends Exception{
+	
+
+	public ProcesoNoPermitido(String mensaje) {
+		super(mensaje);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/logica/utils/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Jorgito
+ *
+ */
+package logica.utils;
